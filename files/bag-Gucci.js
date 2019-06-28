0e9bc51043f28acd28c9d8b4e9a066dd01e8bd36
zhangxitong <zhangxitong@appadhoc.com>
2019-06-28 18:41:03 +0800

    update

---
+++ files/bag-Gucci.js
@@ -0,0 +1,20 @@
+var bagGucciData = [
+    {
+        "name": "我是name很重要",
+        "description": "我是描述,喜欢靠我",
+        "number": "12345",
+        "images": ["http://www.appadhoc.com/images/ec/illustration_10@2x.183888ac.png",
+            "http://www.appadhoc.com/images/ec/illustration_11@2x.690b720b.png",
+            "http://www.appadhoc.com/images/ec/illustration_12@3x.1ca7a386.png"],
+        "note": "我是备注可有可无"
+    },
+    {
+        "name": "我是name很重要",
+        "description": "我是描述,喜欢靠我",
+        "number": "12345",
+        "images": ["http://www.appadhoc.com/images/ec/illustration_10@2x.183888ac.png",
+            "http://www.appadhoc.com/images/ec/illustration_11@2x.690b720b.png",
+            "http://www.appadhoc.com/images/ec/illustration_12@3x.1ca7a386.png"],
+        "note": "我是备注可有可无"
+    }
+];
